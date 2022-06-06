5b1cdf95fa374a5ebd534ae02e737dff76c0565e
CameronJWalker <cameron.jr.walker@icloud.com>
2022-06-05 19:04:52 -0700

    put in api link

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 async function main() {
-
+    let twelveData = await fetch ("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=750af01ad9c746719fc195962e721e81")
+    let getJSON = await twelveData.json()
+    console.log(getJSON);
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
@@ -7,3 +9,9 @@ async function main() {
 }
 
 main()
+
+//  async function getJSON(){
+   // let res = await fetch('https://hackerusa-ce.github.io/example-site/json')
+    //let txt = await res.json()
+   // console.log(txt)
+//}
